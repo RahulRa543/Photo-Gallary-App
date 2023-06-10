aa4699154cd15f6cb88f9f078a49466c216a2485
Rahul Kumar Rana <106945119+RahulRa543@users.noreply.github.com>
2023-06-10 10:47:57 +0530

    Add files via upload

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    "name": "Photo 1",
+    "url": "./images/image1.jpeg",
+    "description": "Photo 1 description",
+    "date": "2020-01-01",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 2",
+    "url": "./images/image2.jpeg",
+    "description": "Photo 2 description",
+    "date": "2020-01-02",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 3",
+    "url": "./images/image3.jpeg",
+    "description": "Photo 3 description",
+    "date": "2020-01-03",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 4",
+    "url": "./images/image4.jpeg",
+    "description": "Photo 4 description",
+    "date": "2020-01-04",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 5",
+    "url": "./images/image5.jpeg",
+    "description": "Photo 5 description",
+    "date": "2020-01-05",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 6",
+    "url": "./images/image6.jpeg",
+    "description": "Photo 6 description",
+    "date": "2020-01-06",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 7",
+    "url": "./images/image7.jpeg",
+    "description": "Photo 7 description",
+    "date": "2020-01-07",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 8",
+    "url": "./images/image8.jpeg",
+    "description": "Photo 8 description",
+    "date": "2020-01-08",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 9",
+    "url": "./images/image9.jpeg",
+    "description": "Photo 9 description",
+    "date": "2020-01-09",
+    "type": "image/jpeg",
+  }
+]
